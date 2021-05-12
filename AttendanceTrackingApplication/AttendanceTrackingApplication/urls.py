9c8e2ba3d079e383590532001348e056d84700a6
palak davda <palakdavda2202@gmail.com>
2021-05-12 21:01:03 +0530

    Django App creating and adding the models

---
+++ AttendanceTrackingApplication/AttendanceTrackingApplication/urls.py
@@ -0,0 +1,29 @@
+"""AttendanceTrackingApplication URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from Attendance_Management import views
+from AttendanceTrackingApplication import settings
+
+urlpatterns = [
+    path('demo',views.showDemoPage),
+    path('admin/', admin.site.urls),
+    path('',views.ShowLoginPage,name="show_login"),
+    path('doLogin',views.doLogin,name="do_login"),
+    path('get_user_details', views.GetUserDetails),
+    path('logout_user', views.logout_user,name="logout"),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
